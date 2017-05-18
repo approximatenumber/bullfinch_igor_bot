95dcd538042bee3c2b506ef7d15516a24939f418
Мельников Максим Алексеевич <mamelnikov@spb.rpkb.ru>
2017-05-18 12:28:20 +0300

    send_statistic()

--- bullfinch_igor_bot.py
+++ bullfinch_igor_bot.py
@@ -3,9 +3,10 @@
 import time
 import pygame
 import pygame.camera
-from telegram import ChatAction, ReplyKeyboardMarkup, ParseMode
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram import ChatAction, ReplyKeyboardMarkup
+from telegram.ext import Updater, CommandHandler
 import configparser
+
 
 class Photographer():
 
@@ -13,51 +14,81 @@ class Photographer():
         self.device = device
         self.resolution = resolution
         self.pic = pic
+        self.photo_count = 0
 
     def start(self, bot, update):
+
         custom_keyboard = [["/now"]]
         reply_markup = ReplyKeyboardMarkup(custom_keyboard, resize_keyboard=True)
         msg = "Добрый день, это бот снегиря Игоря.\n" \
-              "Если вы хотите посмотреть, что сейчас делает Игорь, нажмите на кнопку внизу" \
-              " или просто отправьте /now."
+              "Если вы хотите посмотреть, что сейчас делает Игорь, нажмите на кнопку внизу " \
+              "или просто отправьте /now."
         bot.send_message(chat_id=update.message.chat_id,
                          text=msg, reply_markup=reply_markup)
 
     def send_photo(self, bot, update):
-        bot.sendChatAction(chat_id=update.message.chat_id, action=ChatAction.UPLOAD_PHOTO)
-        self.make_shot()
+
+        def make_shot(self):
+            pygame.camera.init()
+            cam = pygame.camera.Camera(self.device,
+                                       tuple(map(int,
+                                                 self.resolution.split('x'))))
+            cam.start()
+            time.sleep(3)  # need to play with it
+            img = cam.get_image()
+            pygame.image.save(img, self.pic)
+            cam.stop()
+
+        bot.sendChatAction(chat_id=update.message.chat_id,
+                           action=ChatAction.UPLOAD_PHOTO)
+        make_shot()
         photo = open(self.pic, 'rb')
         bot.sendPhoto(chat_id=update.message.chat_id, photo=photo)
+        self.photo_count += 1
 
-    def make_shot(self):
-        pygame.camera.init()
-        cam = pygame.camera.Camera(self.device, 
-                                   tuple(map(int, self.resolution.split('x'))))
-        cam.start()
-        time.sleep(3) # need to play with it
-        img = cam.get_image()
-        pygame.image.save(img, self.pic)
-        cam.stop()
+    def send_statistic(self, bot, update):
+
+        def new_day():
+            current_time = time.localtime()
+            today = current_time.tm_mday
+            hour = current_time.tm_hour
+            if 'lastday' in locals():
+                if today > lastday:
+                    lastday = today
+                    return True
+                else:
+                    return False
+            else:
+                lastday = today
+                return False
+
+        bot.send_message(chat_id=update.message.chat_id, message=self.photo_count)
 
 
-def read_config():
+def read_config(config_file):
 
     config = configparser.ConfigParser()
-    config.read('bullfinch_igor_bot.cfg')
+    config.read(config_file)
 
-    return (config['bot']['token'],
-            config['camera']['device'],
-            config['camera']['resolution'])
+    return {'token':       config['bot']['token'],
+            'device':      config['camera']['device'],
+            'resolution':  config['camera']['resolution']}
 
 
 def main():
 
-    token, device, resolution = read_config()
+    config = read_config('bullfinch_igor_bot.cfg')
+
+    token = config['token']
+    device = config['device']
+    resolution = config['resolution']
+
+    pic = '/tmp/igor.jpg'
 
     # Create the EventHandler and pass it your bot's token.
     updater = Updater(token)
 
-    photographer = Photographer(device, resolution, "/tmp/igor.jpg")
+    photographer = Photographer(device, resolution, pic)
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
@@ -65,9 +96,7 @@ def main():
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", photographer.start))
     dp.add_handler(CommandHandler("now", photographer.send_photo))
-    # dp.add_handler(CommandHandler("help", hetakelp))
-    # dp.add_handler(CommandHandler("tell", tell_weather))
-    # dp.add_handler(CommandHandler("kitty", send_kitty))
+    dp.add_handler(CommandHandler("stat", photographer.send_statistic))
 
     # log all errors
     # dp.add_error_handler(error)
